dfa7647d6fe1079890929f0b4d688c91811d5503
Jasmin Bajrić <jbajric1@etf.unsa.ba>
2019-06-06 16:32:31 +0200

    okk

---
+++ Few Day Stay/Few Day Stay/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Few_Day_Stay.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
