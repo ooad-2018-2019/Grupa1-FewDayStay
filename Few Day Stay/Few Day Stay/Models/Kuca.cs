da6009a142c424b407b20f52cdb3852b35942d2c
Nadija <nborovina1@etf.unsa.ba>
2019-06-07 21:55:06 +0200

    bitmap imported into project

---
+++ Few Day Stay/Few Day Stay/Models/Kuca.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BitmapNet;
+
+namespace FewDayStay.Models
+{
+    public class Kuca : Objekat
+{
+        int brojSpratova;
+        bool imaDvoriste;
+        bool imaBazen;
+        public int BrojSpratova { get => brojSpratova; set => brojSpratova = value; }
+        public bool ImaDvoriste { get => imaDvoriste; set => imaDvoriste = value; }
+        public bool ImaBazen { get => imaBazen; set => imaBazen = value; }
+    }
+}
---
+++ Few Day Stay/Few Day Stay/Models/Objekat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BitmapNet;
+
+namespace FewDayStay.Models
+{
+    public class Objekat
+{
+        int brojKreveta;
+        string naziv;
+        int kvadratura;
+        Lokacija lokacija;
+        double cijenaPoNoci;
+        double ocjena;
+        List<Bitmap> slike;
+        public int BrojKreveta { get => brojKreveta; set => brojKreveta = value; }
+        public string Naziv { get => naziv; set => naziv = value; }
+        public int Kvadratura { get => kvadratura; set => kvadratura = value; }
+        public double CijenaPoNoci { get => cijenaPoNoci; set => cijenaPoNoci = value; }
+        public double Ocjena { get => ocjena; set => ocjena = value; }
+        public Lokacija Lokacija { get => lokacija; set => lokacija = value; }
+
+        public void ocijeni(double ocjena)
+        {
+        }
+}
+}
