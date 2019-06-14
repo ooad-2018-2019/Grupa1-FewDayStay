416b32abd94eac0a528c286aed1c933e410e10ad
Nadija <nborovina1@etf.unsa.ba>
2019-06-14 15:27:13 +0200

    dffj

---
+++ ImplementacijFewDayStay/FewDayStay/FewDayStay/Models/Stan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Few_Day_Stay.Models
+{
+    public class Stan : Objekat
+{
+        int brojSprata;
+        string imeNaUlazu;
+
+        public int BrojSprata { get => brojSprata; set => brojSprata = value; }
+        public string ImeNaUlazu { get => imeNaUlazu; set => imeNaUlazu = value; }
+    }
+}
