42e9232fd988f31d303cef8ad1e335a86895b0ef
Nedzad <nhadzisoma1@etf.unsa.ba>
2019-06-16 18:15:32 +0200

    -jos sekundica i odradjeni viewovi

---
+++ ImplementacijFewDayStay/FewDayStay/FewDayStay/Controllers/ObjaviObjekatController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FewDayStay.Controllers
+{
+    public class ObjaviObjekatController : Controller
+    {
+        public IActionResult ObjaviObjekat()
+        {
+            return View();
+        }
+    }
+}
